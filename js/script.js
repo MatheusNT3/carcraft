eaf3ec442a826d9581f70941176375a845d91f7f
Matheus.dev <matheusmnt13@outlook.com>
2024-01-16 18:40:48 -0300

    inicio javascript no projeto

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// Ativar Links do Menu
+const links = document.querySelectorAll('.header-menu a');
+
+function ativarLink(link) {
+  const url = window.location.href;
+  const href = link.href;
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
+}
+
+links.forEach(ativarLink);
+
+// Ativar Items do Orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+  console.log(elemento);
+}
+
+parametros.forEach(ativarProduto);
+
+// Perguntas Frequentes
