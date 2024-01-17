0ba29d79470706b0f5be7898a095da921637d8fd
Matheus.dev <matheusmnt13@outlook.com>
2024-01-17 17:56:30 -0300

    finalizado o script da galeria de imagens

--- js/script.js
+++ js/script.js
@@ -48,4 +48,22 @@ perguntas.forEach(eventosPerguntas);
 
 console.log(perguntas);
 
-// Galeria de Imagens
+// Galeria de Carros
+
+const galeria = document.querySelectorAll('.carro-imagens img')
+const galeriaContainer = document.querySelector('.carro-imagens');
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia('(min-width: 1000px)').matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+  
+}
+
+function eventosGaleria(img) {
+  img.addEventListener('click', trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
