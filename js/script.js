77b800e2052370e67cfb5cb0b0d9648c6a5d3d70
Matheus.dev <matheusmnt13@outlook.com>
2024-01-17 13:12:23 -0300

    concluído o script de perguntas frequentes.

--- js/script.js
+++ js/script.js
@@ -26,3 +26,26 @@ function ativarProduto(parametro) {
 parametros.forEach(ativarProduto);
 
 // Perguntas Frequentes
+
+const perguntas = document.querySelectorAll('.perguntas button');
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute('aria-controls');
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  console.log('ativa');
+  pergunta.setAttribute('aria-expanded', ativa);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener('click', ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
+
+console.log(perguntas);
+
+// Galeria de Imagens
